8ac5a1261864409e84e834531799562799c97747
Antti Hyytiäinen <hyytant@gmail.com>
2023-11-05 21:59:37 +0200

    pari testia tehty

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,30 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search(self):
+        player = self.stats.search("Lemieux")
+        self.assertEqual(player.name, "Lemieux")
+        player = self.stats.search("Kekke")
+        self.assertEqual(player, None)
+
+    def test_team(self):
+       players = self.stats.team("EDM")
+       self.assertEqual(players[0].team, "EDM")
