9ec3f1c8e79b8e90d977600dd60a275de29c6fc8
Antti Hyytiäinen <hyytant@gmail.com>
2023-11-13 17:35:09 +0200

    Viikko2 tehtävä11

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,6 @@ class Player:
         self.team = dict['team']
         self.goals = dict['goals']
         self.assists = dict['assists']
-
+        self.points = self.goals + self.assists
     def __str__(self):
-        return f"{self.name} team {self.team}"
+        return f"{self.name: <22}{self.team:<4}{self.goals:>3} +{self.assists:>3} ={self.points:>4}"
